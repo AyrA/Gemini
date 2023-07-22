27e9bfee761685cf9d5f4d62acc85ff6381a31be
Kevin Gut <kevin.gut@bitm.sg>
2023-07-22 23:28:18 +0200

    Add application shutdown feature

---
+++ Gemini.Web/Controllers/ServiceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gemini.Web.Controllers
+{
+    [ApiController, Route("[controller]/[action]"), EnableCors("API")]
+    public class ServiceController
+    {
+        private readonly IHostApplicationLifetime _applicationLifetime;
+
+        public ServiceController(IHostApplicationLifetime applicationLifetime)
+        {
+            _applicationLifetime = applicationLifetime;
+        }
+
+        /// <summary>
+        /// Shuts down the current application
+        /// </summary>
+        /// <returns>Always "true"</returns>
+        /// <remarks>
+        /// Shutdown happens approximately 1 second after the request completes
+        /// </remarks>
+        [HttpPost, Produces("application/json")]
+        public bool Shutdown()
+        {
+            Task.Delay(1000).ContinueWith(t =>
+            {
+                _applicationLifetime.StopApplication();
+            });
+            return true;
+        }
+    }
+}
--- Gemini.Web/wwwroot/js/site.js
+++ Gemini.Web/wwwroot/js/site.js
@@ -1,2 +1,22 @@
 ﻿"use strict";
-(function (q, qa, ce) {})(document.querySelector.bind(document), document.querySelectorAll.bind(document), document.createElement.bind(document));
+(function (q, qa, ce) {
+    const dlg = new ezDlg(false);
+    async function stop() {
+        const dlgResult = await dlg.confirm("Really shutdown this application?", "Shutdown", "Shutdown", "Cancel");
+        if (dlgResult) {
+            const response = await fetch("/Service/Shutdown", { method: "POST" });
+            if (response.ok) {
+                document.body.innerHTML = "<h1>Application has been shut down</h1>";
+                await dlg.alert("The application has been shut down. You can close your browser window now.", "Shutdown");
+            }
+            else {
+                await dlg.alert("Call to the API failed. If this continues to happen, open the console window that runs this application and press CTRL+C to trigger a regular application shutdown.", "API failed");
+            }
+        }
+    }
+
+    q("#lnkShutdown").addEventListener("click", function (e) {
+        e.preventDefault();
+        stop();
+    });
+})(document.querySelector.bind(document), document.querySelectorAll.bind(document), document.createElement.bind(document));
