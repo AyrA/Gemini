e49884be3f482fc41e6ef3b9cfdd18dc7935f973
Kevin Gut <kevin.gut@bitm.sg>
2023-07-22 23:45:36 +0200

    Improve run script

--- run.bat
+++ run.bat
@@ -13,12 +13,22 @@ GOTO RNDPORT
 :RNDPORT
 SET PORT=%RANDOM%
 IF %PORT% LSS 10000 GOTO RNDPORT
+GOTO RUN
+
+:RUN
+ECHO Starting application...
+ECHO If your browser won't open,
+ECHO you can manually go to http://localhost:%PORT%
 
 Gemini.Web.exe --urls http://localhost:%PORT%
 GOTO END
 
 :NOBUILD
 ECHO Unable to build the application.
+ECHO Most likely cause is that the .NET 6 SDK is missing.
+ECHO To build this from source, you cannot use the runtime,
+ECHO and must download the SDK version.
+ECHO Go to https://dotnet.microsoft.com/en-us/download to get it.
 PAUSE
 GOTO END
 
